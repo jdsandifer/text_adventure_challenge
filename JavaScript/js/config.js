a72635a26b1a04347ebef9f7356299f959ff189f
jdsandifer <sandifer.jd@gmail.com>
2017-06-03 00:56:14 -0700

    Convert config.js to new JSON style

--- JavaScript/js/config.js
+++ JavaScript/js/config.js
@@ -1,133 +1,212 @@
 // possible inspiration: http://textadventures.co.uk/games/play/5zyoqrsugeopel3ffhz_vq
 
-var array = ['go','look','examine','get','eat','attack','run','flee','use','ctrl+alt+del','inventory'];
-// player attributes
-var player = {
-  health: 100,
-  hunger: 0,
-  inventory: ['knife','snack'],
-  currentRoom: 'room1',
-  moves: 0,
-}
 
-var edibles = {
-  cheese: {
-    id: 'cheese',
-    description: 'A chunk of yellow cheese (maybe cheddar?).',
-    edible: true,
-    healthBonus: 50,
+// Converted to new setup data format as discussed by 
+// Jacob and J.D. on 2017-05-31.
+const setupData = {
+	game: {
+	  name: "the game",
+	  descriptions: ["A Simple Text Adventure Game",
+        "A slightly more complex text adventure game.",
+        "Keep at it! I can make the game harder still..."],
+	  help: "Try simple commands like \"look\", \"go east\", and \"eat banana\". If something doesn't work, try a variant of the command. Good luck!"
   },
-  snack: {
-    id: 'snack',
-    description: 'A health fruit and grain snack bar.',
-    edible: true,
-    healthBonus: 75,
-  },
-  knife: {
-    id: 'knife',
-    description: 'A small pocket knife',
-    edible: false,
-    healthBonus: 0,
-  }
-}
 
-var rooms = {
-  room1: {
-    id: 'room1',
-    name: 'Study room',
-    description: 'Large study with lots dusty old of books. To the east, you see an open door and to the west an hallway.',
-    deepDesc: 'deeper description of room 1.',
-    directions: ['east','west'],
-    connected: ['room2', 'room3'],
-    inventory: ['book'],
-    secret: 'none',
-    objects: [
-      {
-        objectName: 'book',
-        objectDescription: 'An dusty old cook book that looks like its from the 1950s.',
-        canPickup: true,
-      }
-    ]
-  },
-  room2: {
-    id: 'room2',
-    name: 'Kitchen',
-    description: 'Messy kitchen with mold growing the dark corners.',
-    deepDesc: 'deeper description of room 2.',
-    directions: ['north','south'],
-    connected: ['room1', 'room4', 'room5'],
-    inventory: ['toy'],
-    secret: 'none',
-    objects: [
-      {
-        objectId: 'mushroom',
-        objectName: 'smelly brown mushroom',
-        objectDescription: 'Nasty smelling mushroom that looks uninteresting.',
-        canPickup: false,
+  /* room map:
+
+  ****4-------**
+  ****|******|**
+  ****|******|**
+  *6--3--1--2-**
+  **********|***
+  **********5***
+
+  room 1 (Study): go east to room 2 or west to room 3
+  room 2 (Kitchen): go west to room 1 or east (turns north) to room 4, 
+                    secret door that leads to room 5
+  room 3: go east to room 1 or north to room 4
+  room 4: go south to room 3 or east to room 2
+  room 5 (Hidden Nook): only go north back to room 2
+  room 6 (Secret Room): only go east back to room 3
+  */
+
+  // Maybe the first room listed is the starting room?
+  // Then there's no need to have a location for the player anywhere.
+	rooms: [
+    {
+		  name: "Study"
+		  descriptions: ["You are in a large study with lots of dusty old of books. To the east, you see an open door and to the west a hallway."],
+		  doors: ["", "open door", "", "hallway"], // assuming N,E,W,S order for now
+		  entities: {
+			  enemies: [],
+			  neutrals: [],
       },
+      items: ["book"]
+		},
+    {
+		  name: "Kitchen"
+		  descriptions: ["You are in a messy kitchen with mold growing in its dark corners."],
+		  doors: ["corridor", "", "secret door", "open door"],
+        // assuming N,E,W,S order for now
+		  entities: {
+			  enemies: [],
+			  neutrals: [],
+      },
+      items: ["toy", "mushroom"]
+    },
+    {
+		  name: "Room 3"
+		  descriptions: ["There is a door to the north and a hallway to the east."],
+		  doors: ["room 4 door", "hallway", "", "secret passage"],
+        // assuming N,E,W,S order for now
+		  entities: {
+			  enemies: [],
+			  neutrals: [],
+      },
+      items: []
+    },
+    {
+		  name: "Room 4"
+		  descriptions: ["There is a corridor to the east and a door to the south."],
+		  doors: ["", "corridor", "room 4 door", ""],
+        // assuming N,E,W,S order for now
+		  entities: {
+			  enemies: [],
+			  neutrals: [],
+      },
+      items: []
+    },
+    {
+		  name: "Hidden Nook"
+		  descriptions: ["This room is a trap. Player loses the knife and the room picks it up."],
+		  doors: ["secret door", "", "", ""],
+        // assuming N,E,W,S order for now
+		  entities: {
+			  enemies: [],
+			  neutrals: [],
+      },
+      items: []
+    },
+    {
+		  name: "Secret Room"
+		  descriptions: ["You have found a secret room. The only way out is the way you came in (east)."],
+		  doors: ["", "secret passage", "", ""],
+        // assuming N,E,W,S order for now
+		  entities: {
+			  enemies: [],
+			  neutrals: [],
+      },
+      items: []
+    }
+  ],
+
+	doors: [
+    {
+		  name: "open door"
+		  descriptions: ["This door is open."],
+		  connectingRooms: ["Study","Kitchen"]
+		  isLocked: boolean
+		},
+    {
+		  name: "hallway"
+		  descriptions: ["A short hallway"],
+		  connectingRooms: ["Study","Room 3"]
+		  isLocked: boolean,
+    },
+    {
+		  name: "corridor"
+		  descriptions: ["A long, twisting corridor."],
+		  connectingRooms: ["Kitchen","Room 4"]
+		  isLocked: boolean,
+    },
+    {
+		  name: "secret door"
+		  descriptions: ["Shhh! No one's supposed to know about this secret door."],
+		  connectingRooms: ["Kitchen","Hidden Nook"]
+		  isLocked: boolean,
+    },
+    {
+		  name: "room 4 door"
+		  descriptions: ["It's hard to tell what it is..."],
+		  connectingRooms: ["Room 4","Room 3"]
+		  isLocked: boolean,
+    },
+    {
+		  name: "secret passage"
+		  descriptions: ["It's a secret passage!"],
+		  connectingRooms: ["Room 3","Secret Room"]
+		  isLocked: boolean,
+    }
+  ],
+
+	items: [
+    {
+		  name: "cheese",
+		  descriptions: ["A chunk of yellow cheese - maybe cheddar"],
+		  type: "food", // should give 50 hunger units (not implemented)
+		  action: ""
+    },
+    {
+		  name: "snack",
+		  descriptions: ["A health fruit and grain snack bar"],
+		  type: "food", // should give 75 hunger units (not implemented)
+		  action: ""
+    },
+    {
+		  name: "knife",
+		  descriptions: ["A small pocket knife"],
+		  type: "weapon",
+		  action: "cut" // ont sure how actions will work yet
+    },
+    {
+		  name: "book",
+		  descriptions: ["A dusty old cook book that looks like it's from the 1950s"],
+		  type: "item",
+		  action: ""
+    },
+    {
+		  name: "mushroom",
+		  descriptions: ["Nasty smelling mushroom that looks uninteresting"],
+		  type: "item",
+		  action: ""
+    },
+    {
+		  name: "toy",
+		  descriptions: ["A wooden snake toy: It's amazing how flexible it is; very snakelike."],
+		  type: "item",
+		  action: ""
+    }
+  ],
+	entities: {
+    player: {
+      name: "You",
+      descriptions: ["This is you!"],
+      health: 100,
+      hunger: 0,
+      inventory: ["knife","snack"],
+      abilities: [],
+      actionHistory: []
+	  },
+  	enemies: [
       {
-        objectId: 'toy',
-        objectName: 'wooden snake toy',
-        objectDescription: 'Nasty smelling mushroom that looks uninteresting.',
-        canPickup: true,
+        name: "",
+        descriptions: [""],
+        health: 50,
+        inventory: [],
+        abilities: [],
+        actionHistory: []
       }
-    ]
-  },
-  room3: {
-    id: 'room3',
-    description: '...room 3 description...',
-    deepDesc: 'deeper description of room 3.',
-    directions: ['east','north','west'],
-    connected: ['room1', 'room4','room6'],
-    inventory: [],
-    secret: '',
-    objects: [ ]
-  },
-  room4: {
-    id: 'room4',
-    description: '...room 4 description...',
-    deepDesc: 'deeper description of room 4',
-    directions: ['south','east'],
-    connected: ['room3','room2'],
-    inventory: [],
-    secret: '',
-    objects: [ ]
-  },
-  room5: {
-    id: 'room5',
-    description: '...room 5 description... This room is a trap. Player loses the knife and the room picks it up.',
-    deepDesc: 'deeper description of room 5',
-    directions: ['north'],
-    connected: ['room2'],
-    inventory: [],
-    secret: '',
-    objects: [ ]
-  },
-  room6: {
-    id: 'room6',
-    description: '...room 6 description...',
-    deepDesc: 'deeper description of room 6',
-    directions: ['east'],
-    connected: ['room3'],
-    inventory: [],
-    secret: '',
-    objects: [ ]
+	  ],
+	  neutrals: [
+      {
+        name: "",
+        descriptions: [""],
+        health: 100,
+        hunger: 10,
+        inventory: [],
+        abilities: [],
+        actionHistory: []
+      }
+	  ]
   }
 }
-
-/* crude first room map
-
-****4-------**
-****|******|**
-****|******|**
-*6--3--1--2-**
-**********|***
-**********5***
-
-room 1: go east to room 2 or west to room 3
-room 2: go east to room 1 or east (turns north) to room 4
-room 2: secret door that leads to room 5
-room 3: go east to room 1 or north to room 4
-room 4: go south to room 3 or east to room 2
-room 5: only go north back to room 2
-*/
