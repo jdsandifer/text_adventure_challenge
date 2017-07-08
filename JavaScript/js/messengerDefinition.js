4e659b53d50525817c9e9a9bdfc230124c56b473
jdsandifer <sandifer.jd@gmail.com>
2017-07-08 14:11:45 -0700

    Refactor messenger to ES6 class format

--- JavaScript/js/messengerDefinition.js
+++ JavaScript/js/messengerDefinition.js
@@ -1,5 +1,3 @@
-// JD's working on this...
-
 /**
  * For displaying messages to the player
  */
@@ -9,40 +7,38 @@ class Messenger {
     * @param messageArea A dom object in which to place message text
     */
   constructor($messageArea) {
-    this.messageArea = $messageArea   // A dom object in which to place messages
-    this.messageHistory = []    // A list of all previous messages, newest first
-  }
-
-  /**
+    const _messageArea = $messageArea   // A dom object in which to place messages
+    let _messageHistory = []    // A list of all previous messages, newest first
+    
+    /**
     * @param message A string to display to the user
     */
-  addMessage(message) {
-    // Add the new message to the beginning of the history list (FILO)
-    this.messageHistory.unshift(message)
+    this.addMessage = message => {
+      // Add the new message to the beginning of the history list (FILO)
+      _messageHistory.unshift(message)
 
-    // And display all messages
-    this.render()
-  }
+      // And display all messages
+      this.render()
+    }
 
-  clearHistory() {
-    this.messageHistory = []
-  }
+    this.clearHistory = () => _messageHistory = []
 
-  // Display the list of messages
-  render() {
-    // Trim off all but the last ten messages in the history (first ten in array)
-    const MAX_MESSAGES_TO_DISPLAY = 10
-    let lastTenMessages = this.messageHistory.slice(0, MAX_MESSAGES_TO_DISPLAY)
+    // Display the list of messages
+    this.render = () => {
+      // Trim off all but the last ten messages in the history (first ten in array)
+      const MAX_MESSAGES_TO_DISPLAY = 10
+      let lastTenMessages = _messageHistory.slice(0, MAX_MESSAGES_TO_DISPLAY)
 
-    // Create an html message list (using BEM css class structure)
-    let htmlMessages = lastTenMessages.reduce(
-      (acc, message) => acc + `<div class="messages_message">${message}</div>`,
-      ''
-      )
-    let htmlMessagesGroup = `<div class="messages">${htmlMessages}</div>`
+      // Create an html message list (using BEM css class structure)
+      let htmlMessages = lastTenMessages.reduce(
+        (acc, message) => acc + `<div class="messages_message">${message}</div>`,
+        ''
+        )
+      let htmlMessagesGroup = `<div class="messages">${htmlMessages}</div>`
 
-    // Replace current messages with the updated list
-    this.messageArea.children().remove()
-    this.messageArea.append(htmlMessagesGroup)
+      // Replace current messages with the updated list
+      _messageArea.children().remove()
+      _messageArea.append(htmlMessagesGroup)
+    }
   }
 }
